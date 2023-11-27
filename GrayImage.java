3dadb39f85ea34d666a30e5aac326d75b7c860a7
David Yin <29613693+E137299@users.noreply.github.com>
2023-11-27 03:09:05 +0000

    initial commit

---
+++ GrayImage.java
@@ -0,0 +1,26 @@
+public class GrayImage{
+    public static final int BLACK = 0;
+    public static final int WHITE = 255;
+    private int[][] pixelValues;
+   /** The 2-dimensional representation of this image.
+    *  Guaranteed not to be null.
+    *  All values in the array are within the range
+    *  [BLACK, WHITE], inclusive.
+    */
+
+	public GrayImage(int[][] array2D){
+		/* to be implemented  */
+	}
+
+    /** @return the total number of white pixels in
+    *    this image.
+    * Postcondition: this image has not been changed.
+    */
+    public int countWhitePixels(){ 
+	   /* to be implemented in part (a) */ 
+    }
+
+    public int[][] processImage(){
+	   /* to be implemented in part (b) */ 
+    }
+}
